7c9daf7df5b51b0e1b911a43460edd8f7517de82
Arvind Kumar <arvindkumar2575@gmail.com>
2026-02-08 23:36:20 +0530

    add env changes

--- configs/db.php
+++ configs/db.php
@@ -1,12 +1,13 @@
 <?php
+require_once __DIR__ . '/env.php';
 date_default_timezone_set('Asia/Kolkata');
 $visitor_table = "test_visitors";
 
 
-$host = "";
-$db   = "";
-$user = "";
-$pass = "";
+$host = $_ENV['DB_HOSTNAME'];
+$db = $_ENV['DB_NAME'];
+$user   = $_ENV['DB_USERNAME'];
+$pass = $_ENV['DB_PASSWORD'];
 $charset = "utf8mb4";
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
